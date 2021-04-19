8a9beaeeffb063bc4ac6455e406a1a1557675406
Yorgos7784 <ghkd7784@gmail.com>
2021-04-19 16:35:40 +0900

    API 그리드뷰 연습

--- API Practice/API Practice/Form1.cs
+++ API Practice/API Practice/Form1.cs
@@ -17,13 +17,13 @@ namespace API_Practice
         {
             string url = "http://apis.data.go.kr/1470000/DURPrdlstInfoService/getUsjntTabooInfoList"; // URL
             url += "?ServiceKey=" + "Kbg5ok99gLL7L7nQwR3uRoSjPEYTXU2TceiGRQ59%2F6CO%2BzLEl%2F2GcGh4L8BYBXfhtxbTbuUZmGTjb0%2BabDPX6w%3D%3D"; // Service Key
+            url += "&numOfRows=20";
             XElement element = XElement.Load(url);
             foreach (var item in element.Descendants("item"))
             {
                 string durName = item.Element("ITEM_NAME").Value;
                 string durId = item.Element("ITEM_SEQ").Value;
                 string durComp = item.Element("ENTP_NAME").Value;
-
                 durs.Add(new Dur(durName, durId, durComp));
             }
             dataGridView1.DataSource = durs;
