bcb00c3005c037044438aaafe1716a6fc7b90d7c
jhy0409 <inooph@naver.com>
2021-04-21 08:53:16 +0900

    충돌 테스트

---
+++ teamProject/210421_코드 들어오는지 테스트/teamProject/MainForm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace teamProject
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btn_mainDrugComp_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                MessageBox.Show($"{(i+1)} i값이 3번 뜬다");
+            }
+        }
+
+        private void btn_mainDrugs_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
