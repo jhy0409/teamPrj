d6e094f173afd12fd63d075a3d07f24ed042ed9f
jhy0409 <inooph@naver.com>
2021-04-21 09:00:50 +0900

    Update MainForm.cs

--- teamProject/210421_코드 들어오는지 테스트/teamProject/MainForm.cs
+++ teamProject/210421_코드 들어오는지 테스트/teamProject/MainForm.cs
@@ -22,8 +22,9 @@ namespace teamProject
         {
             for (int i = 0; i < 3; i++)
             {
-                MessageBox.Show($"{(i+1)} i값이 3번 뜬다");
+                MessageBox.Show($"{(i + 1)} i값이 3번 뜬다");
             }
+            /* 8:54 AM 별 의미없는 주석.. 코드가 다 들어오는지 보자 */
         }
 
         private void btn_mainDrugs_Click(object sender, EventArgs e)
