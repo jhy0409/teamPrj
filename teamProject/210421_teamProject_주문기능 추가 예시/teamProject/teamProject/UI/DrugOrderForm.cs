76d0a4e7b2455f9c31fcc64a726ad2c2c482a93d
Yorgos7784 <ghkd7784@gmail.com>
2021-04-21 11:53:47 +0900

    주문기능 추가 예시

---
+++ teamProject/210421_teamProject_주문기능 추가 예시/teamProject/teamProject/UI/DrugOrderForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using teamProject.model;
+
+namespace teamProject
+{
+    partial class DrugOrderForm : Form
+    {
+        private List<DrugOrder> orders;
+
+        public DrugOrderForm(List<DrugOrder> orders)
+        {
+            InitializeComponent();
+            this.orders = orders;
+        }
+
+        private void DrugOrderForm_Load(object sender, EventArgs e)
+        {
+            foreach (var item in orders)
+            {
+                drugOrder_dtGridView.Rows.Add(item.Drug.MedName, item.Entp.entpName, item.Count);
+            }
+        }
+    }
+}
---
+++ teamProject/210421_teamProject_주문기능 추가 예시/teamProject/teamProject/UI/MainForm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using teamProject.model;
+
+namespace teamProject
+{
+    public partial class MainForm : Form
+    {
+        List<DrugOrder> lists = new List<DrugOrder>();
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_mainDrugComp_Click(object sender, EventArgs e)
+        {
+            new DrugCompInfo_Form().ShowDialog();
+        }
+
+        private void btn_mainDrugs_Click(object sender, EventArgs e)
+        {
+            new MainDrugs(lists).ShowDialog();
+        }
+
+        private void btn_main_order_Click(object sender, EventArgs e)
+        {
+            new DrugOrderForm(lists).ShowDialog();
+        }
+    }
+}
