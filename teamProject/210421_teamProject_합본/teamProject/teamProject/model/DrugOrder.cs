ed81fcb60551b0cd096b4f61aa02b10cdabfbba1
GarotadeIpanema <75405577+GarotadeIpanema@users.noreply.github.com>
2021-04-21 15:22:56 +0900

    폼1,2,3 합본

---
+++ teamProject/210421_teamProject_합본/teamProject/teamProject/model/DrugOrder.cs
@@ -0,0 +1,20 @@
+﻿namespace teamProject.model
+{
+    class DrugOrder
+    {
+        public Drug drug { get; set; }
+        public Entp entp { get; set; }
+        public int count { get; set; }
+
+        public DrugOrder(Drug drug, Entp entp, int count)
+        {
+            this.drug = drug;
+            this.entp = entp;
+            this.count = count;
+        }
+
+        public int Count { get => count; set => count = value; }
+        internal Drug Drug { get => drug; set => drug = value; }
+        internal Entp Entp { get => entp; set => entp = value; }
+    }
+}
