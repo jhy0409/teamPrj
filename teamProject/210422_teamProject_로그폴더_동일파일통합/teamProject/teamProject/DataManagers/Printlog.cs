2d17922b83f695502c3c5c5c53e3b8b5998a0a50
jhy0409 <inooph@naver.com>
2021-04-22 10:23:05 +0900

    로그 클래스 dataManager - dataManager_Comp & dataManager

---
+++ teamProject/210422_teamProject_로그폴더_동일파일통합/teamProject/teamProject/DataManagers/Printlog.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teamProject.DataManagers
+{
+    class Printlog
+    {
+
+        public string contents { get; set; }
+        public string date { get; set; }
+
+        public static Printlog prl;
+
+        public static void printLog(string contents, string date)
+        {
+            DirectoryInfo di = new DirectoryInfo("History");
+            if (!di.Exists)
+            {
+                di.Create();
+            }
+
+            using (StreamWriter writer = new StreamWriter(@"History\" + date + ".txt", true))
+            {
+                writer.WriteLine($"[ {date} ]\t{contents}");
+            }
+        }
+
+        public static Printlog getInst()
+        {
+            if (prl == null)
+            {
+                prl = new Printlog();
+            }
+            return prl;
+        }
+
+    }
+}
